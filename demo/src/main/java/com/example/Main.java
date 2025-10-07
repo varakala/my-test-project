dda3159d88349366fb907b26389c983f2b1a5b23
kiran <varakala_k2k@hotmail.com>
2025-10-06 20:26:24 -0400

    First: Mac
    commited from Mac book

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Addition: " + SampleFunc.add(5, 3)  );
+        System.out.println("Multiplication: " + SampleFunc.multiply(5, 3) );
+        System.out.println("Subtraction: " + SampleFunc.subtract(5, 3) );
+    }
+}
---
+++ demo/src/main/java/com/example/SampleFunc.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class SampleFunc {
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
